70fe69440f5fa53f8a4a606399cbb6f31110664e
一纸忘忧 <i@ikxin.com>
2026-05-19 14:01:45 +0800

    feat: 引入 Semi Design 组件库

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	transpilePackages: ["@douyinfe/semi-ui", "@douyinfe/semi-icons", "@douyinfe/semi-illustrations"],
 };
 
 export default nextConfig;
---
+++ src/instrumentation-client.ts
@@ -0,0 +1,1 @@
+import "@douyinfe/semi-ui/react19-adapter";
